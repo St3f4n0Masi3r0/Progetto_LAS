d2cb470e3c9f0122f69f682749045df977bc4eb1
Max Nov <maksym.nov@gmail.com>
2023-05-18 19:20:07 +0200

    Angular App not working in docker
    Co-authored-by: Stefano Masiero <St3f4n0Masi3r0@users.noreply.github.com>

--- webserver/frontend/src/app/app.component.spec.ts
+++ webserver/frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test_app'`, () => {
+  it(`should have as title 'las-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test_app');
+    expect(app.title).toEqual('las-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('test_app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('las-app app is running!');
   });
 });
---
+++ webserver/frontend/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
+@Component({
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  template: `
+    <h1>{{ message }}</h1>
+  `,
+  
+})
+
+
+export class AppComponent implements OnInit {
+  title = 'las-app';
+  message: string = 'hey';
+
+  
+  constructor(private http: HttpClient) {}
+
+  
+
+  
+  ngOnInit() {
+    this.http.get<any>('http://webserver1:3000').subscribe((response) => {
+      this.message = response.message;
+    });
+  }
+}
